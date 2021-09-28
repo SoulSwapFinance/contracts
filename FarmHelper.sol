6e091a033efacdb92e7f5c5223fa0f404571b320
DeGatchi <72078332+DeGatchi@users.noreply.github.com>
2021-09-29 08:14:52 +1000

    fix: WETH tvl

--- FarmHelper.sol
+++ FarmHelper.sol
@@ -48,8 +48,8 @@ contract FarmHelper {
             if (token0 == FUSD || token0 == USDC)  {
                 poolTvl = IToken(token0).balanceOf(lpToken) * 2;
             } else {
-                poolTvl = IToken(token1).balanceOf(lpToken) * 2
-            ;
+                poolTvl = IToken(token1).balanceOf(lpToken) * 2;
+            }
         } else if (token0 == WFTM || token1 == WFTM) {
             if (token0 == WFTM) {
                 poolTvl = IToken(token0).balanceOf(lpToken) * 2;
